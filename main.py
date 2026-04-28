63b86f9261f709ab663adc4e7421623e78c0289f
LeoCrge <leo.carouge7@gmail.com>
2026-04-28 23:27:31 +0200

    Project Setup

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def readfile(file):
+    path="src/Problems/Problem" + str(file) + ".txt"
+    with open(path, 'r') as f:
+        lines = [line.strip() for line in f.readlines() if line.strip()]
+    firstLine = lines[0].split()
+    numProvision = int(firstLine[0])
+    numOrders = int(firstLine[1])
+    print(numProvision, numOrders)
+    cost_matrix = []
+    provisions = []
+    orders = []
+
+    for i in range(1, numProvision + 1):
+        parts = lines[i].split()
+        row_costs = [int(x) for x in parts[:numOrders]]
+        cost_matrix.append(row_costs)
+
+        provisions.append(int(parts[-1]))
+    orders_line =  lines[numProvision + 1].split()
+    orders = [int(x) for x in orders_line]
+
+    print("Suppliers (n):", numProvision)
+    print("Customers (m):", numOrders)
+    print("Cost Matrix:", cost_matrix)
+    print("Provisions:", provisions)
+    print("Orders:", orders)
+    return numProvision, numOrders, cost_matrix, provisions, orders
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    readfile("2")
